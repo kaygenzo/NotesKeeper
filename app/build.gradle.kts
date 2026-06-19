291adee66d63b96f2436727c017a3ea7109a1733
Karim Yarboua <karim.yarboua@gmail.com>
2026-06-19 22:09:49 +0200

    Use Java 17 instead of 21
    The docker image is using java 17 for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
 
@@ -114,7 +114,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
+        jvmTarget.set(JvmTarget.JVM_17)
     }
 }
 
